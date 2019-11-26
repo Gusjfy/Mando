5cf767752e650ddea40560dc857f9cd898d654e5
Gusjfy <gustavoj.salvalaggio@gmail.com>
2019-11-25 21:42:33 -0300

    Teste git

--- app/src/main/java/com/first/authetication/MessageActivity.java
+++ app/src/main/java/com/first/authetication/MessageActivity.java
@@ -43,7 +43,8 @@ public class MessageActivity extends AppCompatActivity {
         //Return the intent that started this activity.
         intent = getIntent();
         String userId = intent.getStringExtra("userid");
-
+        // comentario
+        int teste = 15;
         fuser = FirebaseAuth.getInstance().getCurrentUser();
 
     }
