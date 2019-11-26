5be5b2955af64957f9e7de9d8297fbc43fe65d1e
Gusjfy <gustavoj.salvalaggio@gmail.com>
2019-11-25 22:20:11 -0300

    Inicio da implementação do chat

--- app/src/main/java/com/first/authetication/MessageActivity.java
+++ app/src/main/java/com/first/authetication/MessageActivity.java
@@ -1,5 +1,6 @@
 package com.first.authetication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -9,9 +10,14 @@ import android.widget.EditText;
 import android.widget.TextView;
 import androidx.appcompat.widget.Toolbar;
 
+import com.first.authetication.model.User;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MessageActivity extends AppCompatActivity {
 
@@ -42,7 +48,27 @@ public class MessageActivity extends AppCompatActivity {
         username = findViewById(R.id.username);
         //Return the intent that started this activity.
         intent = getIntent();
+
+        //No código onde vai estar o usuário e irá chamar essa activity, colocar esse código
+        // mContext = getContext();
+        //Intent intent = new Intent(mContext, MessageActivity.class);
+        //intent.putExtra("userid", user.getId());
+        //mContext.startActivity(intent);
         String userId = intent.getStringExtra("userid");
+        fuser = FirebaseAuth.getInstance().getCurrentUser();
+        reference = FirebaseDatabase.getInstance().getReference("users").child(userId);
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                User user = dataSnapshot.getValue(User.class);
+                username.setText(user.getNome());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
 
     }
 }
