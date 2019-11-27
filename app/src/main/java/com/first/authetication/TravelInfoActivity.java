287169336054ed5fe117ddb9209d57961990dd4d
Leonardo Steinke <leonardosteinke1@gmail.com>
2019-11-27 01:44:29 -0300

    MT MASSA

--- app/src/main/java/com/first/authetication/TravelInfoActivity.java
+++ app/src/main/java/com/first/authetication/TravelInfoActivity.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.first.authetication.model.Travel;
 import com.first.authetication.model.User;
@@ -26,26 +27,58 @@ public class TravelInfoActivity extends AppCompatActivity {
 
     private Button btnChat;
 
+    TextView txtNome, txtOrigem, txtDestino, txtValor, txtData, txtHora;
+
+
     FirebaseUser firebaseUser;
     //    DatabaseReference referenceUser;
     DatabaseReference reference;
+    DatabaseReference referenceUser;
     Travel travel;
+    User user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_travel_info);
 
+        txtNome = findViewById(R.id.txtNomeEntregador);
+        txtOrigem = findViewById(R.id.txtOrigem);
+        txtDestino = findViewById(R.id.txtDestino);
+        txtValor = findViewById(R.id.txtValor);
+        txtData = findViewById(R.id.txtData);
+        txtHora = findViewById(R.id.txtHora);
+
+
+
         intent = getIntent();
         String cod = intent.getStringExtra("travelid");
-
-        Log.d("vsf", cod);
-
         reference = FirebaseDatabase.getInstance().getReference("travel").child(cod);
+
+
         reference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 travel = dataSnapshot.getValue(Travel.class);
+                txtOrigem.setText(travel.getOrigem());
+                txtDestino.setText(travel.getDestino());
+                txtValor.setText("R$" + travel.getValor());
+                txtData.setText(travel.getData());
+                txtHora.setText(travel.getHora());
+                referenceUser = FirebaseDatabase.getInstance().getReference("users").child(travel.getId_entregador());
+                referenceUser.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        user = dataSnapshot.getValue(User.class);
+                        txtNome.setText(user.getNome());
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+
 
             }
 
