8cfd9ea6cd6000950cca2e8101cd396fe89af258
Gusjfy <gustavoj.salvalaggio@gmail.com>
2019-11-26 23:15:10 -0300

    Mandando mensagem

--- app/src/main/java/com/first/authetication/MessageActivity.java
+++ app/src/main/java/com/first/authetication/MessageActivity.java
@@ -7,7 +7,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.appcompat.widget.Toolbar;
 
 import com.first.authetication.model.User;
@@ -19,6 +22,8 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.HashMap;
+
 public class MessageActivity extends AppCompatActivity {
 
     private TextView username;
@@ -28,30 +33,45 @@ public class MessageActivity extends AppCompatActivity {
 
     private Intent intent;
 
+    private ImageButton btn_send;
+    private EditText text_send;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_message);
 
         Toolbar toolbar = findViewById(R.id.toolbar);
-
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         toolbar.setNavigationOnClickListener(new View.OnClickListener(){
-
             @Override
             public void onClick(View v) {
                 finish();
             }
         });
         username = findViewById(R.id.username);
-        //Return the intent that started this activity.
+        btn_send = findViewById(R.id.btn_send);
+        text_send = findViewById(R.id.text_send);
+
         intent = getIntent();
-
-
-        String userId = intent.getStringExtra("userid");
+        final String userId = intent.getStringExtra("userid");
         fuser = FirebaseAuth.getInstance().getCurrentUser();
+
+        btn_send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String msg = text_send.getText().toString();
+                if (!msg.equals("")){
+                    sendMessage(fuser.getUid(), userId, msg);
+                } else {
+                    Toast.makeText(MessageActivity.this, "Você não pode mandar uma mensagem vazia", Toast.LENGTH_SHORT).show();
+                }
+                text_send.setText("");
+            }
+        });
+
         reference = FirebaseDatabase.getInstance().getReference("users").child(userId);
         reference.addValueEventListener(new ValueEventListener() {
             @Override
@@ -66,5 +86,16 @@ public class MessageActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    private void sendMessage(String sender, String receiver, String message){
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
+
+        HashMap<String, Object> hashMap = new HashMap<>();
+        hashMap.put("sender", sender);
+        hashMap.put("receiver", receiver);
+        hashMap.put("message", message);
+
+        reference.child("chats").push().setValue(hashMap);
     }
 }
